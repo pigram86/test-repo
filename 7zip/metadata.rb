7f1e1c2f2630a9dbe3e5647c17587fb00512667f
pigram86 <todd@toddpigram.com>
2013-12-19 20:07:55 -0500

    safe keeping

--- 7zip/metadata.rb
+++ 7zip/metadata.rb
@@ -4,11 +4,5 @@ maintainer_email 'todd@toddpigram.com'
 license          'Apache 2.0'
 description      'Installs/Configures 7zip'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-<<<<<<< HEAD
 version          '1.1.0'
 depends          "windows"
-=======
-version          '0.1.0'
-depends          "windows"
-
->>>>>>> d69eb37d11c0503075661b052fcf9c9f01b35586
